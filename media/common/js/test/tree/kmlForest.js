3447f4ead2f285b08c3b3a6009792e5505a9e848
underbluewaters <devnull@localhost>
2009-08-03 18:16:46 +0000

    Added unimplemented tests for kmlForest

--- media/common/js/test/tree/kmlForest.js
+++ media/common/js/test/tree/kmlForest.js
@@ -1,1 +1,78 @@
 module('kmlForest');
+
+test('initialize', function(){
+    
+});
+
+test('add', function(){
+    
+});
+
+test('remove', function(){
+    
+});
+
+test('refresh', function(){
+    
+});
+
+test('getByUrl', function(){
+    
+});
+
+test('appropriate category header', function(){
+    
+});
+
+test('supports kml <a href="http://code.google.com/apis/kml/documentation/kmlreference.html#open">open tag</a>', function(){
+    
+});
+
+test('supports <a href="http://code.google.com/apis/kml/documentation/kmlreference.html#visibility">visibility tag</a>', function(){
+    
+});
+
+test('supports <a href="http://code.google.com/apis/kml/documentation/kmlreference.html#snippet">snippet tag</a>', function(){
+    
+});
+
+test('toggle on/off features', function(){
+    
+});
+
+test('toggle on/off folders and NetworkLinks', function(){
+    
+});
+
+test('features with descriptions have balloon link', function(){
+    
+});
+
+test('double click feature flys to feature', function(){
+    
+});
+
+test('proper default icons given to folders, network links, features, etc', function(){
+    
+});
+
+test('<a href="http://code.google.com/apis/kml/documentation/kmlreference.html#liststyle">ListStyle</a> support: radioFolder. All ListStyle support depends on <a href="http://code.google.com/p/earth-api-samples/issues/detail?id=303">this ticket</a>', function(){
+    
+});
+
+test('<a href="http://code.google.com/apis/kml/documentation/kmlreference.html#liststyle">ListStyle</a> support: checkOffOnly', function(){
+    
+});
+
+test('<a href="http://code.google.com/apis/kml/documentation/kmlreference.html#liststyle">ListStyle</a> support: checkHideChildren', function(){
+    
+});
+
+test('<a href="http://code.google.com/apis/kml/documentation/kmlreference.html#liststyle">ListStyle</a> support: ItemIcon', function(){
+    
+});
+
+test('Contents of NetworkLinks can be displayed. Depends on <a href="http://code.google.com/p/earth-api-samples/issues/detail?id=260&q=NetworkLink&colspec=ID%20Type%20Summary%20Component%20OpSys%20Browser%20Status%20Stars#c3">this ticket</a>, or a hack', function(){
+    
+});
+
