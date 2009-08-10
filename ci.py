8324b75ea807162bf8425e0b2599705d1440f17f
underbluewaters <devnull@localhost>
2009-08-10 19:44:14 +0000

    Added option to build javascript docs using jsdoc toolkit. Just specify the directory that jsrun.jar can be found in when calling ci.py.

--- ci.py
+++ ci.py
@@ -9,10 +9,13 @@ import sys
 import os
 from optparse import OptionParser
 
-usage = "usage: %prog -d PATH | --docs-output=PATH"
+usage = "usage: %prog -j PATH -d PATH | --docs-output=PATH"
 parser = OptionParser(usage)
 parser.add_option('-d', '--docs-output', dest='docs_output', metavar='PATH',
                 help="Where you would like the generated documentation to be moved.")
+
+parser.add_option('-j', '--jar-path', dest='jar', metavar='PATH',
+                help="Location of the jsdoc toolkit jar")
                   
 (options, args) = parser.parse_args()
 
@@ -25,6 +28,13 @@ os.system('cd docs; make clean; make html')
 if options.docs_output:
     os.system('rm -rf %s/*' % (options.docs_output, ))
     os.system('cp -r docs/.build/html/* %s/' % (options.docs_output, ))
+
+    if options.jar:
+        print "Generating javascript documentation"
+        c = 'java -jar %s/jsrun.jar %s/app/run.js -a -t=%s/templates/jsdoc media/common/js -r=10 -d=%s/jsdocs' % (options.jar, options.jar, options.jar, options.docs_output, )
+        print c
+        os.system(c)
+    
 
 # Remove settings_local if it exists
 try:
