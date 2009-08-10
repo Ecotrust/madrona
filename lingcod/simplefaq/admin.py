f2728a0b567623fd36572fac753a47fef9574298
vormooth <devnull@localhost>
2009-08-10 18:15:36 +0000

    usable admin for simplefaq

--- lingcod/simplefaq/admin.py
+++ lingcod/simplefaq/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from lingcod.simplefaq.models import Faq, FaqGroup
 
-admin.site.register(Faq)
+class FaqAdmin(admin.ModelAdmin):
+    search_fields = ('question', 'answer')
+    list_display = ('question','importance','faq_group')   
+    
+admin.site.register(Faq,FaqAdmin)
 admin.site.register(FaqGroup)
