dd583de8148cb37f1ab558c5d5726d109cb22161
underbluewaters <devnull@localhost>
2009-08-28 17:51:30 +0000

    referencing simple_app from simple. When installing this project using Apache, both lingcod and simple are added to site-packages, but the documentation does not list simple_app. To keep things simple (h^ h^), lets reference simple_app from simple so there doesn't have to be another addition to site-packages.

--- example_projects/simple/urls.py
+++ example_projects/simple/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     (r'^studyregion/', include('lingcod.studyregion.urls')),
     (r'^faq/', include('lingcod.simplefaq.urls')),
     (r'^manipulators/', include('lingcod.manipulators.urls')),
-    (r'^simple-app/', include('simple_app.urls')),
+    (r'^simple-app/', include('simple.simple_app.urls')),
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
