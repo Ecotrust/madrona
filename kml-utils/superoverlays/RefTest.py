65441bbb3a8b2e1413032660ac59798bcdd639fc
underbluewaters <devnull@localhost>
2009-10-09 18:40:05 +0000

    Added utility folder for all our kml generation scripts

---
+++ kml-utils/superoverlays/RefTest.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import mapnik
+from georeference import render_to_wld
+
+mapfile = 'Habitat.xml'
+
+m = mapnik.Map(20000,20000,'+proj=latlong +datum=WGS84')
+
+mapnik.load_map(m, mapfile)
+
+lyr = mapnik.Layer('world',"+proj=latlong +datum=WGS84")
+lyr.datasource = mapnik.Shapefile(file='./data/BroadScaleHabsProj.shp')
+m.zoom_to_box(lyr.envelope())
+filepath = 'map'
+raster = '%s.png' % filepath
+world_file = '%s.pngw' % filepath
+mapnik.render_to_file(m, raster, 'png')
+render_to_wld(m, world_file)
+
+
+
