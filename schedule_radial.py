392885aa4806565cf0df39f5b2a41420336f0bec
underbluewaters <devnull@localhost>
2009-10-12 20:01:34 +0000

    Added radial "neato" type graphviz output for tickets that have no links to other tickets.

---
+++ schedule_radial.py
@@ -0,0 +1,67 @@
+"""
+Fetches tickets from the marinemap issue tracker and generates a dot file for 
+generating a diagram using graphviz
+"""
+
+import urllib
+f = urllib.urlopen('http://code.google.com/p/marinemap/issues/csv?can=1&q=&colspec=ID%20Type%20Status%20Priority%20Owner%20Component%20BlockedOn%20EstimatedTime%20Summary')
+
+
+def get_ticket(id):
+    for ticket in tickets:
+        if ticket['id'] == id:
+            return ticket
+    return None
+
+import csv
+reader = csv.reader(f, delimiter=',', quotechar='"')
+print """
+graph graphname {
+   ratio=0.4
+"""
+tickets = []
+
+tickets_that_block = []
+
+for row in reader:
+    if reader.line_num != 1 and row:
+        ticket = {
+            'id': int(row[0]),
+            'type': row[1],
+            'status': row[2],
+            'priority': row[3],
+            'owner': row[4],
+            'component': row[5],
+            'blockedon': row[6],
+            'estimatedtime': row[7],
+            'summary': row[8]
+        }
+        blockers = [tid.strip() for tid in ticket['blockedon'].split(',')]
+        ticket['blockedon'] = []
+        for blocker in blockers:
+            if blocker != '':
+                blocker = int(blocker)
+                ticket['blockedon'].append(blocker)
+                tickets_that_block.append(blocker)
+        if ticket['component'] != 'KmlTools':
+            tickets.append(ticket)
+# 
+# for ticket in tickets:
+#     for blocker in ticket['blockedon']:
+#         print '%s -> %s;' % (blocker, ticket['id'])
+
+for ticket in tickets:
+    if (len(ticket['blockedon'])) == 0 and ticket['id'] not in tickets_that_block:
+        fillcolor = 'palegoldenrod'
+        fontcolor = 'black'
+        style= 'filled'
+                    
+        if ticket['status'] == 'WontFix' or ticket['status'] == 'Fixed' or ticket['status'] == 'Done':
+            fillcolor = 'lightslategray'
+            fontcolor = 'black'
+        if ticket['status'] == 'Started':
+            fillcolor = 'palegreen'
+            
+        print '%s [URL="http://code.google.com/p/marinemap/issues/detail?id=%s",label="%s\\n%s\\n%s\\n%s\\nStatus: %s\\nOwner: %s",fontsize=24,fillcolor=%s,fontcolor=%s, style=%s];' % (ticket['id'], ticket['id'], ticket['summary'], ticket['estimatedtime'], ticket['priority'], ticket['component'], ticket['status'], ticket['owner'], fillcolor, fontcolor, style)
+        
+print "}"
