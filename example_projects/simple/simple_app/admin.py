aa0d066fbfecac22c08b4253164e66cc24da5bcd
perry <perry@marinemap.org>
2009-11-17 17:48:36 +0000

    Adding Arrays to the admin site

--- example_projects/simple/simple_app/admin.py
+++ example_projects/simple/simple_app/admin.py
@@ -1,5 +1,7 @@
 from lingcod.mpa.admin import MpaAdmin
+from lingcod.array.admin import ArrayAdmin
 from django.contrib.gis import admin
-from simple_app.models import Mpa
+from simple_app.models import Mpa, MpaArray
 
 admin.site.register(Mpa, MpaAdmin)
+admin.site.register(MpaArray, ArrayAdmin)
