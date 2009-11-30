68faf93e405d5e5000a4659a3678d3b681735507
underbluewaters <devnull@localhost>
2009-11-30 23:07:10 +0000

    Added user_mpa_kml named url

--- lingcod/kmlapp/urls.py
+++ lingcod/kmlapp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from django.core.urlresolvers import reverse
 
 urlpatterns = patterns('lingcod.kmlapp.views',
-    (r'^(?P<input_username>\w+)/user_mpa.kml$', 'create_mpa_kml' ),
+    url(r'^(?P<input_username>\w+)/user_mpa.kml$', 'create_mpa_kml', name='user_mpa_kml' ),
     (r'^(?P<input_array_id>\d+)/array.kml$', 'create_mpa_kml' ),
     (r'^(?P<input_mpa_id>\d+)/mpa.kml$', 'create_mpa_kml' ),
     (r'^(?P<input_username>\w+)/user_mpa.kmz$', 'create_mpa_kmz' )
