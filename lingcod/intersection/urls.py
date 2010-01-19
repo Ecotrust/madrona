bf0501cfb51a217da444b73218aee457cdef0f04
underbluewaters <devnull@localhost>
2010-01-19 15:58:33 +0000

    login url in the intersection app was competing with nc_mlpa's login url. Disabling.

--- lingcod/intersection/urls.py
+++ lingcod/intersection/urls.py
@@ -4,7 +4,7 @@ from views import *
 urlpatterns = patterns('',
     #(r'multifeatureshapefile/splitbyfield/$',upload_intersection_feature),
     
-    (r'^login', 'django.contrib.auth.views.login'),
+    # (r'^login', 'django.contrib.auth.views.login'),
     (r'^intersect/testdrawing/$', test_drawing_intersect ),
     (r'^intersect/testpolygon/$', test_poly_intersect),    
     (r'^info/organization_scheme/(\d+)$', org_scheme_info ),
