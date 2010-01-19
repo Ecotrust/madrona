295bff98b5b3ebb04c56d5378ad8a3ad53a26255
jkibele <devnull@localhost>
2010-01-19 17:23:35 +0000

    apparently, there needs to be something in here so here's something that doesn't do anything.

--- lingcod/data_distributor/urls.py
+++ lingcod/data_distributor/urls.py
@@ -1,6 +1,6 @@
-# from django.conf.urls.defaults import *
-# from views import *
-#   
-# urlpatterns = patterns('',
-#     url(r'^load_potential_targets', load_potential_targets_view, name='load_potential_targets'),
-# )
+from django.conf.urls.defaults import *
+from views import *
+  
+urlpatterns = patterns('',
+    url(r'^nothing', nothing, name='nothing'),
+)
