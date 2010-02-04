624f688e080ddfac00cdef5e66611b143732c953
perry <perry@marinemap.org>
2010-02-04 22:24:33 +0000

    Utility shortcut for sharing app to get an object by id; if it fails, returns useful httpresponse with proper error code

---
+++ lingcod/sharing/utils.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse, Http404
+from models import *
+
+def get_viewable_object_or_respond(model_class, pk):
+    """
+    Gets a single instance of model_class
+    if it doesnt exist or user can't access it,
+    return 404 or 403
+    """
+    # Does it even exist?
+    try:
+        obj = model_class.objects.get(pk=pk)
+    except:
+        raise Http404
+
+    obj = None
+    try:
+        # Next see if user owns it
+        obj = model_class.objects.get(id=input_mpa_id, user=user).add_kml()
+    except model_class.DoesNotExist:
+        try: 
+            # ... finally see if its shared with the user
+            obj = model_class.objects.shared_with_user(user).get(id=input_mpa_id).add_kml()
+        except Mpa.DoesNotExist:
+            return HttpResponse("Access denied", status=403)
+
+    return obj
+
