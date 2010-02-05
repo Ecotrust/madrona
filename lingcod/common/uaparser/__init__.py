f776a453c1227a0861d8799eb79f5a35b9b73b54
perry <perry@marinemap.org>
2010-02-05 19:06:32 +0000

    Fix type error for different version numbering schemes in uaparser

--- lingcod/common/uaparser/__init__.py
+++ lingcod/common/uaparser/__init__.py
@@ -1,14 +1,20 @@
 import regexes, parser
 import clientos
 
+def int_or_zero(x):
+    try:
+        return int(x)
+    except TypeError:
+        return 0
+
 class browser_platform():
     def __init__(self,ua):
         a = parser.UserAgent(ua)
         b = clientos.client_os(ua) 
         self.family = a.family
-        self.v1 = int(a.v1)
-        self.v2 = int(a.v2)
-        self.v3 = int(a.v3)
+        self.v1 = int_or_zero(a.v1)
+        self.v2 = int_or_zero(a.v2)
+        self.v3 = int_or_zero(a.v3)
         self.platform = b['platform']
         self.full_platform = b['full_platform']
     
--- lingcod/common/uaparser/tests.py
+++ lingcod/common/uaparser/tests.py
@@ -5,6 +5,7 @@ uas = [
        ('Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_2; en-us) AppleWebKit/531.21.8 (KHTML, like Gecko) Version/4.0.4 Safari/531.21.10',True),
        ('Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_2; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.49 Safari/532.5',False),
        ('Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6',False),
+       ('Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6',False),
       ]
 
 for ua in uas:
