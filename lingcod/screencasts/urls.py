2c82c3ac00fd1c1e3830c94754ddd1c97ddc1ab4
perry <perry@marinemap.org>
2010-02-18 19:03:57 +0000

    Screencast/Help app to use swf video player instead of linking to flv

--- lingcod/screencasts/urls.py
+++ lingcod/screencasts/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('lingcod.screencasts.views',   
     (r'^$', 'listTutorials'),  
     (r'^(\w+)/$', 'showVideo'),
+    url(r'^(?P<pk>\d)/show/$', 'showVideoByPk', name='screencasts-show-video'),
 )
--- lingcod/screencasts/views.py
+++ lingcod/screencasts/views.py
@@ -8,12 +8,28 @@ from django.conf import settings
 def listTutorials(request, screencasts_template='tutorials.html'):
     return render_to_response(screencasts_template, context_instance=RequestContext(request, {'MEDIA_URL':settings.MEDIA_URL, 'screencast_list':Screencast.objects.all()})) 
 
-
 def showVideo(request, urlname, demo_template='demo_video.html'):
     try:
         screencast = Screencast.objects.get(urlname=urlname)
     except:
         return HttpResponse( "Screencast " + urlname + " does not exist.", status=404 )
+
+    return render_to_response(demo_template, {'videoplayer':settings.VIDEO_PLAYER, 'screencast':screencast}, context_instance=RequestContext(request))
+
+
+def showVideoByPk(request, pk, demo_template='demo_video.html'):
+    try:
+        screencast = Screencast.objects.get(pk=pk)
+    except:
+        return HttpResponse( "Screencast does not exist.", status=404 )
+
+    if not settings.VIDEO_PLAYER:
+        return HttpResponse( "Server error - VIDEO_PLAYER is not defined.", status=500 )
+
+    import os
+    player_path = settings.MEDIA_ROOT + "../" + settings.VIDEO_PLAYER
+    if not os.path.exists(player_path):
+        return HttpResponse( "Server error - VIDEO_PLAYER does not exist <br/> should live at %s" % settings.VIDEO_PLAYER, status=500 )
     
     return render_to_response(demo_template, {'videoplayer':settings.VIDEO_PLAYER, 'screencast':screencast}, context_instance=RequestContext(request)) 
 
