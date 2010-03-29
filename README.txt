32a8484c9160050617908e480d9b1e99edb95f6b
perry <perry@marinemap.org>
2010-03-29 22:45:05 +0000

    Initial import of the setup script. Should now be able to run running develop running egg_info writing marinemap.egg-info/PKG-INFO writing top-level names to marinemap.egg-info/top_level.txt writing dependency_links to marinemap.egg-info/dependency_links.txt writing manifest file 'marinemap.egg-info/SOURCES.txt' running build_ext Creating /Library/Python/2.6/site-packages/marinemap.egg-link (link to .) marinemap 1.2 is already the active version in easy-install.pth
    Installed /Users/perry/src/marinemap/trunk
    Processing dependencies for marinemap==1.2
    Finished processing dependencies for marinemap==1.2 instead of symlinking code. Eventually we'll publish to PyPi.

--- README.txt
+++ README.txt
@@ -1,9 +1,28 @@
-To generate the documentation, go to docs/ and run::
 
-    make html
+MarineMap
+==========
 
-To run the test suite, go to example-projects/simple and run::
-    
-    python manage.py test
-    
-See the project website at http://code.google.com/p/marinemap for more info.
+.. image:: http://marinemap.org/mm_logo_only.gif
+   :width: 213
+   :height: 130
+
+MarineMap is a web-based decision support tool for open and participatory spatial 
+planning in the marine environment. MarineMap offers a simple, flexible and powerful
+means of gathering expertise from resource managers, scientists, stakeholders and 
+public in a process of collaborative decision making.
+
+MarineMap is developed as a set of reusable apps for the django web framework. 
+The components are bundled into a python module called 'lingcod'. Using lingcod, 
+one can create their own implementation of MarineMap or extend the tools for any
+sort of collaborative spatial planning process. 
+
+For more details, see:
+
+* MarineMap documentation_
+* MarineMap home_
+* MarineMap `code site`_
+
+
+.. _home: http://marinemap.org
+.. _code site: http://code.google.com/p/marinemap/ 
+.. _documentation: http://maps11.msi.ucsb.edu/marinemap-docs/
--- setup.py
+++ setup.py
@@ -1,20 +1,45 @@
-from distutils.core import setup
+import warnings
 
-setup(name='marinemap',
-      version='0.1',
-      description='A framework for building decisison support tools supporting marine spatial planning',
-      author='MarineMap Consortium',
-      author_email='mcclintock@msi.ucsb.edu',
-      url='http://code.google.com/p/marinemap',
-      # download_url='http://code.google.com/p/marinemap',
-      packages=['lingcod'],
-      license='New BSD License',
-      # classifiers=['Development Status :: 4 - Beta',
-      #              'Environment :: Web Environment',
-      #              'Framework :: Django',
-      #              'Intended Audience :: Developers',
-      #              'License :: OSI Approved :: BSD License',
-      #              'Operating System :: OS Independent',
-      #              'Programming Language :: Python',
-      #              'Topic :: Utilities'],
-      )
+try:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+except:
+    warnings.warn(
+    "Failed to import distribute_setup, continuing without distribute.", 
+    Warning)
+
+from setuptools import setup, find_packages
+
+readme_text = file('README.txt', 'rb').read()
+
+setup_args = dict(
+    name                = 'marinemap',
+    version             = '1.2',
+    #requires_python     = '>=2.5,<3',
+    #requires_external  = 
+    description         = 'A framework for building decisison support tools supporting marine spatial planning',
+    author              = 'MarineMap Consortium',
+    author_email        = 'mcclintock@msi.ucsb.edu',
+    maintainer          = 'MarineMap Consortium',
+    maintainer_email    = 'mcclintock@msi.ucsb.edu',
+    url                 = 'http://code.google.com/p/marinemap',
+    license             = 'New BSD License',
+    keywords            = 'kml marine decisionsupport science gis',
+    long_description    = readme_text,
+    packages            = ['lingcod.%s' % x for x in find_packages('lingcod')],
+    #scripts            = 
+    #test_suite         = 
+    classifiers         = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: GIS',
+        'Framework :: Django',
+        'Environment :: Web Development'
+        ],
+    )
+
+setup(**setup_args)
