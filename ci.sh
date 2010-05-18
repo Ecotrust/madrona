9f6554568b1b467d58fc9935961219a7805e28cd
Matthew Perry <perrygeo@gmail.com>
2010-05-18 11:42:37 -0600

    Test first, build docs later. Faster test feedback from hudson.

--- ci.sh
+++ ci.sh
@@ -2,17 +2,17 @@
 # $JOB_NAME
 echo 
 echo 
-echo Building MarineMap docs : revision $MERCURIAL_REVISION
-echo
-echo
-python build_docs.py -d /var/www/marinemap-docs -j  /usr/local/jsdoc-toolkit-read-only/jsdoc-toolkit 
-
-echo 
-echo 
 echo Testing MarineMap : revision $MERCURIAL_REVISION
 echo
 echo
 coverage run run_tests.py
+
+echo 
+echo 
+echo Building MarineMap docs : revision $MERCURIAL_REVISION
+echo
+echo
+python build_docs.py -d /var/www/marinemap-docs -j  /usr/local/jsdoc-toolkit-read-only/jsdoc-toolkit 
 
 echo 
 echo 
