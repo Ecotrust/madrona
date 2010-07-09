5bc45236a4537e968a790d93a78c8a16d32077f3
sfletche <sfletche@gmail.com>
2010-07-09 11:37:02 -0700

    committing some changes to async so that i can update lingcod...wish there was a way around having to commit before updating...also wish there was a way to update a particular directory or file...

--- lingcod/async/models.py
+++ lingcod/async/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from djcelery.models import TaskMeta
 
 '''
 NOTES:
     url entries should be unique -- a single url maps to a single task_id in a one-to-one fashion
-    we'll want to make sure task is present in celery table, maybe verify on save?
 '''
 class URLtoTaskID(models.Model):
     url = models.TextField(verbose_name='Polling Url')
     task_id = models.TextField(verbose_name='task_id from celery_taskmeta')
+
