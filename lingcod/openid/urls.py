954d234e549876e9ed9c599a11642be76fff7a0e
Matthew Perry <perrygeo@gmail.com>
2010-08-30 15:50:51 -0600

    Fixed openid/registration interactions; modified templates for a nic(er) UI flow

--- lingcod/openid/urls.py
+++ lingcod/openid/urls.py
@@ -21,16 +21,18 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from lingcod.openid import views as oid_views
 from registration import views as reg_views
+from registration.views import activate
+from registration.views import register
 
 
 urlpatterns = patterns('',
     # django registration activate
-    url(
-        r'^activate/(?P<activation_key>\w+)/$',
-        reg_views.activate,
-        {'backend': 'registration.backends.default.DefaultBackend'},
-        name='registration_activate'
-    ),
+#    url(
+#        r'^activate/(?P<activation_key>\w+)/$',
+#        reg_views.activate,
+#        {'backend': 'registration.backends.default.DefaultBackend'},
+#        name='registration_activate'
+#    ),
     
     # user profile
     
@@ -65,7 +67,36 @@ urlpatterns = patterns('',
     url(r'^signup/complete/$',direct_to_template, 
         {'template': 'registration/registration_complete.html'},
         name='registration_complete'),
+
+    url(r'^activate/complete/$',
+        direct_to_template,
+        { 'template': 'registration/activation_complete.html' ,
+            'extra_context': {'group_request_email': settings.GROUP_REQUEST_EMAIL}
+        },
+        name='registration_activation_complete'),
+    # Activation keys get matched by \w+ instead of the more specific
+    # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
+    # that way it can return a sensible "invalid key" message instead of a
+    # confusing 404.
+    url(r'^activate/(?P<activation_key>\w+)/$',
+        activate,
+        { 'backend': 'lingcod.common.registration_backend.LingcodBackend' },
+        name='registration_activate'),
+#    url(r'^register/$',
+#        register,
+#        { 'backend': 'lingcod.common.registration_backend.LingcodBackend' },
+#        name='registration_register'),
+#    url(r'^register/complete/$',
+#        direct_to_template,
+#        { 'template': 'registration/registration_complete.html'},
+#        name='registration_complete'),
+    url(r'^register/closed/$',
+        direct_to_template,
+        { 'template': 'registration/registration_closed.html' },
+        name='registration_disallowed'),
         
     # yadis uri
     url(r'^yadis.xrdf$', oid_views.xrdf, name='oid_xrdf'),
 )
+
+
