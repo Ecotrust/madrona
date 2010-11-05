5fb5442a20eba6ba640f74c6a540632bfecac4f8
Chad Burt <underbluewaters@gmail.com>
2010-11-05 08:08:45 -0700

    fixing deprecated paths in TEMPLATE_CONTEXT_PROCESSORS

--- lingcod/common/default_settings.py
+++ lingcod/common/default_settings.py
@@ -140,7 +140,7 @@ SHARING_TO_STAFF_GROUPS = ['Share with Staff']
 
 # TEMPLATE SETTINGS
 TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.auth",
+    "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
