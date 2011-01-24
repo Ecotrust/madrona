564d607c86e91f2fd14d101f7d349e1c205438fd
Matthew Perry <perrygeo@gmail.com>
2011-01-24 10:19:41 -0700

    Remove rest import from test_project urls

--- example_projects/test_project/urls.py
+++ example_projects/test_project/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-from lingcod import rest
 
 admin.autodiscover()
 
