fbc887e5ea63cbe3171e6907f6dd31c6acaa9421
Matthew Perry <perrygeo@gmail.com>
2011-02-04 12:26:14 -0700

    Set up docs and setup.py and settings so that there is only one place we need to change the version number (lingcod.common.default_settings.RELEASE)

--- lingcod/common/default_settings.py
+++ lingcod/common/default_settings.py
@@ -4,7 +4,7 @@ import os
 # DONT FORGET TO DOCUMENT ANY NEW SETTINGS IN /docs/settings.rst
 # !!!!!!!!!!!!!!!!!!!!!!
 
-RELEASE = '1.9dev' # The next milestone
+RELEASE = '2.0dev' # The next milestone
 
 GEOMETRY_DB_SRID = 3310
 
