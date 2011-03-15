ac4b5a6d7ad177ba12c936a396b1aee7363bd603
Chad Burt <underbluewaters@gmail.com>
2011-03-15 12:23:55 -0700

    updated example workspace to match output of workspace_json

--- media/features/test/example_workspace.js
+++ media/features/test/example_workspace.js
@@ -30,87 +30,134 @@ var doc = {
       "select": "multiple single", 
       "rel": "alternate", 
       "title": "KML"
+    }, 
+    {
+      "title": "Remove", 
+      "models": [
+        "mlpa_array", 
+        "mlpa_folder"
+      ], 
+      "uri-template": "/features/generic-links/links/remove/{id+}/", 
+      "rel": "edit", 
+      "method": "POST", 
+      "select": "multiple single"
+    }, 
+    {
+      "title": "Add", 
+      "models": [
+        "mlpa_array", 
+        "mlpa_folder"
+      ], 
+      "uri-template": "/features/generic-links/links/add/{id+}/", 
+      "rel": "edit", 
+      "method": "POST", 
+      "select": "multiple single"
     }
   ], 
   "feature-classes": [
     {
       "link-relations": {
+        "edit": [
+          {
+            "uri-template": "/features/shipwreck/{id}/form/", 
+            "title": "edit"
+          }
+        ], 
         "self": {
-          "title": 'Attributes',
           "uri-template": "/features/shipwreck/{id}/"
         }, 
         "create": {
           "uri-template": "/features/shipwreck/form/"
-        }, 
-        "edit": [
-            {
-              "title": "Attributes and Geometry",
-              "uri-template": "/features/shipwreck/{id}/form/"
-            }
-        ]
+        }
       }, 
       "id": "mlpa_shipwreck", 
       "title": "Shipwreck"
     }, 
     {
       "link-relations": {
+        "edit": [
+          {
+            "uri-template": "/features/mpa/{id}/form/", 
+            "title": "edit"
+          }
+        ], 
         "self": {
-        "title": 'Attributes',
-            
           "uri-template": "/features/mpa/{id}/"
         }, 
         "create": {
           "uri-template": "/features/mpa/form/"
-        }, 
-        "edit": [
-            {
-              "title": "Attributes and Geometry",
-              "uri-template": "/features/mpa/{id}/form/"
-            }
-        ]
+        }
       }, 
       "id": "mlpa_mpa", 
       "title": "Marine Protected Area"
     }, 
     {
       "link-relations": {
+        "edit": [
+          {
+            "uri-template": "/features/array/{id}/form/", 
+            "title": "edit"
+          }
+        ], 
         "self": {
-            "title": 'Attributes',
-            
           "uri-template": "/features/array/{id}/"
         }, 
         "create": {
           "uri-template": "/features/array/form/"
-        }, 
-        "edit": [
-            {
-              "title": "Attributes and Geometry",
-              "uri-template": "/features/array/{id}/form/"
-            }
-        ]
+        }
       }, 
       "id": "mlpa_array", 
+      "collection": {
+        "classes": [
+          "mlpa_mpa"
+        ]
+      }, 
       "title": "Array"
     }, 
     {
       "link-relations": {
+        "edit": [
+          {
+            "uri-template": "/features/pipeline/{id}/form/", 
+            "title": "edit"
+          }
+        ], 
         "self": {
-            "title": 'Attributes',
-            
           "uri-template": "/features/pipeline/{id}/"
         }, 
         "create": {
           "uri-template": "/features/pipeline/form/"
-        }, 
-        "edit": [
-            {
-              "title": "Attributes and Geometry",
-              "uri-template": "/features/pipeline/{id}/form/"
-            }
-        ]
+        }
       }, 
       "id": "mlpa_pipeline", 
       "title": "Pipeline"
+    }, 
+    {
+      "link-relations": {
+        "edit": [
+          {
+            "uri-template": "/features/folder/{id}/form/", 
+            "title": "edit"
+          }
+        ], 
+        "self": {
+          "uri-template": "/features/folder/{id}/"
+        }, 
+        "create": {
+          "uri-template": "/features/folder/form/"
+        }
+      }, 
+      "id": "mlpa_folder", 
+      "collection": {
+        "classes": [
+          "mlpa_mpa", 
+          "mlpa_array", 
+          "mlpa_pipeline", 
+          "mlpa_shipwreck", 
+          "mlpa_folder"
+        ]
+      }, 
+      "title": "Folder"
     }
   ]
 };
