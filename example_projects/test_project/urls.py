54b9c9cf3f4c90bf961f305974076cf72a084243
Matthew Perry <perrygeo@gmail.com>
2011-03-28 16:00:10 -0600

    Include kmlapp in default urls

--- example_projects/test_project/urls.py
+++ example_projects/test_project/urls.py
@@ -8,7 +8,6 @@ urlpatterns = patterns('',
     url(r'^marinemap/', 'django.views.generic.simple.redirect_to', {'url': '/'}),    
     url(r'^$', 'lingcod.common.views.map', {'template_name': 'common/map_ext.html'}, name="map"),    
     (r'^tests/', 'django.views.generic.simple.direct_to_template', {'template': 'common/tests.html', 'extra_context': {'api_key': settings.GOOGLE_API_KEY}}),
-    (r'^kml/', include('lingcod.kmlapp.urls')),
     
     # Include all lingcod app urls. Any urls above will overwrite the common 
     # urls below
