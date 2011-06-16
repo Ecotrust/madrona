eeadbe2b5b878429ab7d5bc3c2cc5dc9d00888e5
Matthew Perry <mperry@ecotrust.org>
2011-06-16 10:18:40 -0700

    improvements to enable_sharing

--- lingcod/features/management/commands/enable_sharing.py
+++ lingcod/features/management/commands/enable_sharing.py
@@ -1,19 +1,52 @@
 from django.core.management.base import BaseCommand, AppCommand
 from optparse import make_option
+from django.contrib.auth.models import Group
 from lingcod.common.utils import enable_sharing 
 
 
 class Command(BaseCommand):
     help = "Configures the permissions to allow sharing between groups"
+    args = '[Group names to grant sharing permissions to ...]'
 
-    def handle(self, **options):
+    option_list = BaseCommand.option_list + (
+        make_option('-a', '--all', action='store_true', dest='all',
+            help="Enable sharing for ALL current groups"),
+        )
+    
+
+    def handle(self, *groupnames, **options):
+        self.all = options.get('all', False)
+
+        if self.all:
+            print "Enabling sharing for all groups..."
+            enable_sharing()
+            gs = Group.objects.all()
+            for g in gs:
+                enable_sharing(g)
+                print " [DONE]", g.name
+            return
+
+        if len(groupnames) > 0:
+            print "Enabling sharing for %s groups.." % len(groupnames)
+            enable_sharing()
+            for gname in groupnames:
+                try:
+                    g = Group.objects.get(name=gname)
+                    enable_sharing(g)
+                    print " [DONE]", gname
+                except Exception as e:
+                    print " [FAILED]", gname
+                    print "  ",e
+            return 
+
         enable_sharing()
         print """
-The site is now configured to allow sharing. To grant this permission to any groups, go to the shell and type:
+The site is now configured to allow sharing. 
+For a group to share features, you must grant this permission explictly to group:
 
-    from lingcod.common.utils import enable_sharing
-    from django.contrib.auth.models import Group
-    g = Groups.objects.get(name="My Group")
-    enable_sharing(g)
+    $ python manage.py enable_sharing GroupName "Group Name with Spaces"
 
+OR to grant sharing permissions to all groups:
+
+    $ python manage.py enable_sharing --all
 """
