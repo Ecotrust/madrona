9643d9a7f30642fe92e2360d1fba15c511ac0f39
Matthew Perry <perrygeo.code@gmail.com>
2011-08-12 11:48:02 -0700

    Adding categorical raster support to raster_stats app; requires migration

--- lingcod/raster_stats/models.py
+++ lingcod/raster_stats/models.py
@@ -30,6 +30,13 @@ class RasterDataset(models.Model):
     def __unicode__(self):
         return unicode(self.name + " raster at " + self.filepath)
     
+class ZonalCategory(models.Model):
+    category = models.IntegerField()
+    count = models.IntegerField()
+
+    def __unicode__(self):
+        return unicode("Category %s (count = %s)" % (self.category, self.count))
+
 class ZonalStatsCache(models.Model):
     geom_hash = models.CharField(max_length=255)
     raster = models.ForeignKey('RasterDataset')
@@ -42,6 +49,7 @@ class ZonalStatsCache(models.Model):
     stdev = models.FloatField(null=True, blank=True)
     nulls = models.FloatField(null=True, blank=True)
     pixels = models.FloatField(null=True, blank=True)
+    categories = models.ManyToManyField(ZonalCategory)
     date_modified = models.DateTimeField(auto_now=True)
 
     @property
@@ -66,7 +74,7 @@ def geom_to_file(geom, filepath):
     fh.close()
     assert os.path.exists(filepath)
 
-def run_starspan_zonal(geom, rasterds, write_cache=False):
+def run_starspan_zonal(geom, rasterds, write_cache=False, pixprop=0.5):
     """
     Consider this a 'private' method .. dont call directly, use zonal_stats() instead
     Runs starspan and returns a ZonalStatsCache object
@@ -83,12 +91,18 @@ def run_starspan_zonal(geom, rasterds, write_cache=False):
 
     # Run starspan
     out_csv = os.path.join(tmpdir, 'output_%s_stats.csv' % timestamp)
+    out_categories = os.path.join(tmpdir, 'output_%s_categories.csv' % timestamp)
     if os.path.exists(out_csv):
         os.remove(out_csv)
-    cmd = '%s --vector %s --where "id=1" --out-prefix %s/output_%s --out-type table --summary-suffix _stats.csv --raster %s --stats avg mode median min max sum stdev nulls ' % (STARSPAN_BIN,out_json,tmpdir, timestamp, rasterds.filepath)
+    cmd = '%s --vector %s --where "id=1" --out-prefix %s/output_%s --out-type table --summary-suffix _stats.csv --raster %s --stats avg mode median min max sum stdev nulls --pixprop %s ' % (STARSPAN_BIN,out_json,tmpdir, timestamp, rasterds.filepath, pixprop)
+    if rasterds.type == 'categorical':
+        cmd += "--class-summary-suffix _categories.csv"
+
     starspan_out = os.popen(cmd).read()
 
     if not os.path.exists(out_csv):
+        raise Exception("Starspan failed to produce output file: %s" % starspan_out)
+    if rasterds.type == 'categorical' and not os.path.exists(out_categories):
         raise Exception("Starspan failed to produce output file: %s" % starspan_out)
 
     res = open(out_csv,'r').readlines()
@@ -111,6 +125,20 @@ def run_starspan_zonal(geom, rasterds, write_cache=False):
             elif headers[i] == 'numPixels':
                 zonal.pixels = float(vals[i])
 
+    # Handle categories
+    if rasterds.type == 'categorical' and zonal.pixels:
+        zonal.save()
+        res = open(out_categories).readlines()
+        headers = [x.strip() for x in res[0].split(',')]
+        assert headers == ['FID', 'class', 'count']
+        for row in res[1:]:
+            vals = [int(x.strip()) for x in row.split(',')]
+            cat = vals[1]
+            count = vals[2]
+            zc = ZonalCategory.objects.create(category=cat, count=count)
+            zc.save()
+            zonal.categories.add(zc)
+
     # return zonal object (caching it if needed)
     if write_cache:
         try:
@@ -126,7 +154,7 @@ def clear_cache():
     objs = ZonalStatsCache.objects.all()
     objs.delete()
 
-def zonal_stats(geom, rasterds, write_cache=True, read_cache=True, cache_only=False):
+def zonal_stats(geom, rasterds, write_cache=True, read_cache=True, cache_only=False, pixprop=0.5):
     """
     Given a GEOSGeometry and a RasterDataset,
     compute the zonal stats and return json like
@@ -143,14 +171,14 @@ def zonal_stats(geom, rasterds, write_cache=True, read_cache=True, cache_only=Fa
     if read_cache:
         try:
             cached = ZonalStatsCache.objects.get(geom_hash=hash, raster=rasterds)
-        #except (ZonalStatsCache.DoesNotExist, DatabaseError):
         except ZonalStatsCache.DoesNotExist:
             cached = None
         except DatabaseError:
             cached = None
-            #cached = ZonalStatsCache(geom_hash=hash, raster=rasterds)
     else:
-        write_cache = False #If we're not reading the cache, we're not going to write to it either
+        #If we're not reading the cache, 
+        #we're not going to write to it either
+        write_cache = False 
 
     if cached:
         result = cached
@@ -160,7 +188,7 @@ def zonal_stats(geom, rasterds, write_cache=True, read_cache=True, cache_only=Fa
             # Return an empty result
             result = ZonalStatsCache(geom_hash=hash, raster=rasterds)
         else:
-            result = run_starspan_zonal(geom, rasterds, write_cache=write_cache)
+            result = run_starspan_zonal(geom, rasterds, write_cache=write_cache, pixprop=pixprop)
         result.from_cache = False
     
     return result
