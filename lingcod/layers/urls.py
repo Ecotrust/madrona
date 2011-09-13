e7f97bf1ddcf05bee3c3b6fc79c5cefb36af280a
Matthew Perry <perrygeo.code@gmail.com>
2011-09-13 14:34:11 -0700

    Add another url pattern for debugging public layers

--- lingcod/layers/urls.py
+++ lingcod/layers/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls.defaults import *
+import time
 
 
 urlpatterns = patterns('lingcod.layers.views',
-    url(r'^public/', 
+    url(r'^public/$', 
         'get_public_layers', 
         name='public-data-layers'),
+
+    # Useful for debugging, avoids GE caching interference
+    url(r'^public/cachebuster/%s' % str(time.time()), 
+        'get_public_layers', 
+        name='public-data-layers-cachebuster'),
+
     url(r'^kml_file/(?P<session_key>\w+)/(?P<uid>[\w_]+).kml', 
         'get_kml_file', 
         name='kml-file'),
