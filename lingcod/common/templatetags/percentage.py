f90e4cf48245fe493b33171a60fc268435cb3a81
sfletche <sfletche@gmail.com>
2011-11-03 16:04:43 -0700

    changing the percentage templatetag so it handles precision parameter

--- lingcod/common/templatetags/percentage.py
+++ lingcod/common/templatetags/percentage.py
@@ -2,7 +2,8 @@ from django import template
 
 register = template.Library()
 
-def percentage(value):
-    return '{0:.0%}'.format(value)
+def percentage(decimal, precision=0):
+    format_string = "." + str(precision) + "%"
+    return format(decimal, format_string)
 
 register.filter('percentage', percentage)
