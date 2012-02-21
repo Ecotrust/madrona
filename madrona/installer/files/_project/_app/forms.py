9422cba596b2ca9217198d4eeecac3ea1b29f952
Matthew Perry <mperry@ecotrust.org>
2012-02-21 15:21:11 -0800

    Provide example features and feature collections in models and forms

---
+++ madrona/installer/files/_project/_app/forms.py
@@ -0,0 +1,13 @@
+
+################ EXAMPLE ##########################################
+#from madrona.features.forms import FeatureForm, SpatialFeatureForm
+#from _app.models import AOI, Folder
+#
+#class AOIForm(SpatialFeatureForm):
+#    class Meta(SpatialFeatureForm.Meta):
+#        model = AOI
+#
+#class FolderForm(FeatureForm):
+#    class Meta(FeatureForm.Meta):
+#        model = Folder
+################ END EXAMPLE ######################################
--- madrona/installer/files/_project/_app/models.py
+++ madrona/installer/files/_project/_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from madrona.features import register
 
-# Create your models here.
+################## EXAMPLE ###########################################
+#from madrona.features.models import PolygonFeature, FeatureCollection
+#
+#@register
+#class AOI(PolygonFeature):
+#    class Options:
+#        form = '_project._app.forms.AOIForm'
+#
+#@register
+#class Folder(FeatureCollection):
+#    class Options:
+#        form = '_project._app.forms.FolderForm'
+#        valid_children = (
+#            '_project._app.models.AOI',
+#            '_project._app.models.Folder',
+#        )
+################## END EXAMPLE #######################################
