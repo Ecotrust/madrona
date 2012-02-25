3a99cfaf3008b30e2acf91161ae15f54996ec4b5
Matthew Perry <mperry@ecotrust.org>
2012-02-24 17:37:57 -0800

    Install cleangeometry actually installs the cleangeometry function

--- madrona/common/management/commands/install_cleangeometry.py
+++ madrona/common/management/commands/install_cleangeometry.py
@@ -7,12 +7,12 @@ class Command(BaseCommand):
 
     def handle(self, **options):
         path = os.path.abspath(os.path.join(__file__, '../../../cleangeometry.sql'))
-        print """
 
-        It's too hard to do this using the django connection interface automatically, but here's how you can do it yourself:
+        sql = open(path,'r').read()
+        # http://stackoverflow.com/questions/1734814/why-isnt-psycopg2-
+        # executing-any-of-my-sql-functions-indexerror-tuple-index-o        
+        sql = sql.replace('%','%%')
 
-        cat %s | psql %s -U %s
-
-        just copy and paste that into your terminal and you are done!
-
-        """ % (path, connection.settings_dict['NAME'], connection.settings_dict['USER'])
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        print cursor.statusmessage
