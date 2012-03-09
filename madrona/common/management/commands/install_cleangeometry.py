105111b0ed02a7c698ba79f88a54636ec3d5b2a8
Matthew Perry <mperry@ecotrust.org>
2012-03-09 10:40:02 -0800

    Use transactions to make sure cleangeometry function sticks

--- madrona/common/management/commands/install_cleangeometry.py
+++ madrona/common/management/commands/install_cleangeometry.py
@@ -14,5 +14,15 @@ class Command(BaseCommand):
         sql = sql.replace('%','%%')
 
         cursor = connection.cursor()
+        cursor.db.enter_transaction_management()
+
         cursor.execute(sql)
         print cursor.statusmessage
+        print "TESTING"
+
+        cursor.execute("select cleangeometry(st_geomfromewkt('SRID=4326;POLYGON ((30 10, 10 20, 20 40, 40 40, 30 10))'))")
+        assert cursor.fetchall() == [('0103000020E610000001000000050000000000000000003E4000000000000024400000000000002440000000000000344000000000000034400000000000004440000000000000444000000000000044400000000000003E400000000000002440',)]
+        cursor.db.commit()
+        cursor.db.leave_transaction_management()
+        print "CLEANGEOMETRY function installed successfully"
+
