2eaba12224965a58dd2707ac79c0ca6fd127308d
Matthew Perry <mperry@ecotrust.org>
2012-03-19 18:13:17 -0700

    Update absurl tag accd to gist 945391

--- madrona/common/templatetags/absurl.py
+++ madrona/common/templatetags/absurl.py
@@ -1,22 +1,38 @@
 import urlparse
 from django.template import Library
-from django.template.defaulttags import URLNode
-from django.templatetags.future import url
+from django.templatetags.future import url, URLNode
 from django.contrib.sites.models import Site
 
 register = Library()
 
 class AbsoluteURLNode(URLNode):
+    def __init__(self, view_name, args, kwargs, asvar, legacy_view_name=True):
+        super(AbsoluteURLNode, self).__init__(view_name, 
+            args, 
+            kwargs, 
+            None, 
+            legacy_view_name)
+        self.abs_asvar = asvar
+    
     def render(self, context):
         path = super(AbsoluteURLNode, self).render(context)
         domain = "http://%s" % Site.objects.get_current().domain
-        return urlparse.urljoin(domain, path)
+        url = urlparse.urljoin(domain, path)
 
-def absurl(parser, token, node_cls=AbsoluteURLNode):
+        
+        if self.abs_asvar:
+            context[self.abs_asvar] = url
+            return ''
+        else:
+            return url
+        
+def absurl(parser, token):
     """Just like {% url 'urlname' %} but ads the domain of the current site."""
     node_instance = url(parser, token)
-    return node_cls(view_name=node_instance.view_name,
-        args=node_instance.args,
+    return AbsoluteURLNode(view_name=node_instance.view_name, 
+        args=node_instance.args, 
         kwargs=node_instance.kwargs,
-        asvar=node_instance.asvar)
+        asvar=node_instance.asvar,
+        legacy_view_name=node_instance.legacy_view_name)
+
 absurl = register.tag(absurl)
