607f3b11aa6bee7b3c4cf60e38f624a5249c6485
Matthew Perry <perrygeo@gmail.com>
2012-03-31 17:51:34 -0700

    import feedvalidator from madrona

--- madrona/common/feedvalidator/formatter/base.py
+++ madrona/common/feedvalidator/formatter/base.py
@@ -9,7 +9,7 @@ __copyright__ = "Copyright (c) 2002 Sam Ruby and Mark Pilgrim"
 from UserList import UserList
 import os
 LANGUAGE = os.environ.get('LANGUAGE', 'en')
-lang = __import__('feedvalidator.i18n.%s' % LANGUAGE, globals(), locals(), LANGUAGE)
+lang = __import__('madrona.common.feedvalidator.i18n.%s' % LANGUAGE, globals(), locals(), LANGUAGE)
 
 from feedvalidator.logging import Info, Warning, Error
 
