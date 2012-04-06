dbb389d8c958ebbbe23cdbce3f19da65ef5c91e6
Matthew Perry <mperry@ecotrust.org>
2012-04-06 09:55:45 -0700

    Added cachemethod decorator

--- madrona/common/utils.py
+++ madrona/common/utils.py
@@ -622,3 +622,24 @@ def is_text(s):
     if float(len(t)) / len(s) > 0.30:
         return False
     return True
+
+from django.core.cache import cache
+def cachemethod(cache_key, timeout=3600):
+    '''
+    http://djangosnippets.org/snippets/1130/    
+    Cacheable class method decorator
+    from madrona.common.utils import cachemethod
+    @cachemethod("SomeClass_get_some_result_%(id)s")
+    '''
+    def paramed_decorator(func):
+        def decorated(self):
+            key = cache_key % self.__dict__
+            res = cache.get(key)
+            if res == None:
+                res = func(self)
+                cache.set(key, res, timeout)
+            return res
+        decorated.__doc__ = func.__doc__
+        decorated.__dict__ = func.__dict__
+        return decorated 
+    return paramed_decorator
