6f5d9b961693897d19453d1f3cea06fd06d4b72b
Matthew Perry <mperry@ecotrust.org>
2012-04-09 16:54:43 -0700

    urls to get admin media right

--- madrona/installer/files/_project/urls.py
+++ madrona/installer/files/_project/urls.py
@@ -1,7 +1,20 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+import django
+import os
+
+django_dir = os.path.dirname(django.__file__)
+urlpatterns = patterns('',
+    (r'^media/admin/(.*)', 
+        'django.views.static.serve', 
+        {
+         'document_root' : os.path.join(django_dir, 'contrib', 'admin', 'static', 'admin'), 
+         'show_indexes' : False
+        }
+    ),
+)
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns += patterns('',
     (r'', include('madrona.common.urls')),
 )
