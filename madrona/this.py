c05a0ab0ed42c0c61b60c8e592af21804b3b3e9e
Matthew Perry <perrygeo@gmail.com>
2012-06-03 11:50:46 -0700

    The Zen of madrona; from madrona import this

---
+++ madrona/this.py
@@ -0,0 +1,11 @@
+s = """
+The Zen of Madrona
+
+The best decision-making process will vary by geography and culture.
+Work directly with the intended users to capture the mental model of your decision-making process.
+Let the mental model define which features are needed.
+The Features will then define the API.
+The API and its workspace document will drive the client interface.
+Let these conventions be your guide but they should get out of the way when no longer useful."""
+
+print s
