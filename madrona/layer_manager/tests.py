b290ef1127e83c5fc3d9c031c97cb4d34f6bf394
Matthew Perry <mperry@ecotrust.org>
2012-07-19 13:24:08 -0700

    tweak test

--- madrona/layer_manager/tests.py
+++ madrona/layer_manager/tests.py
@@ -1,38 +1,42 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from django.test.client import Client
-from madrona.layer_manager import *
 from madrona.layer_manager.models import Theme, Layer
 import simplejson
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
 
 class LayerManagerTest(TestCase):
     def setUp(self):
         self.client = Client()
+        self.url = '/layer_manager/get_json'
         self.theme1 = Theme.objects.create(name="Theme1")
-        self.layer1 = Layer.objects.create(name="Layer1", layer_type='XYZ', url='https://s3.amazonaws.com/marco-public-2d/Test/ocsb2/ocsb/${z}/${x}/${y}.png')
+        self.layer1 = Layer.objects.create(name="Layer1", layer_type='XYZ', 
+                url='https://example.com/ocsb1/ocsb/${z}/${x}/${y}.png')
         self.layer1.themes.add(self.theme1)
-        self.client.login(username='layertest', password='pword')
+        self.layer2 = Layer.objects.create(name="Layer2", layer_type='XYZ', 
+                url='https://example.com/ocsb2/ocsb/${z}/${x}/${y}.png')
+        self.layer2.themes.add(self.theme1)
 	
-    def test_layer_manager(self):	
-        url = '/layer_manager/get_json'
-        response = self.client.get(url)
+    def test_themes(self):	
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200, response.status_code)
-        res_str = response.content + ','
-        obj = simplejson.loads('[%s]' % res_str[:-1])
-        self.assertEqual(obj[0]["themes"][0]["layers"][0], self.layer1.id, obj[0]["themes"][0]["layers"][0])
-        self.assertEqual(obj[0]["state"]["activeLayers"], [], obj[0]["state"]["activeLayers"])
-        self.assertEqual(obj[0]["themes"][0]["name"], "Theme1", obj[0]["themes"][0]["name"])
-        self.assertEqual(obj[0]["themes"][0]["layers"], [1], obj[0]["themes"][0]["layers"])
+        res_str = response.content
+        obj = simplejson.loads(res_str)
+        self.assertEqual(obj["state"]["activeLayers"], [], obj)
+        self.assertEqual(obj["themes"][0]["name"], "Theme1", obj)
+        self.assertEqual(obj["themes"][0]["layers"], [self.layer1.id, self.layer2.id], obj)
+
+    def test_state(self):	
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200, response.status_code)
+        res_str = response.content
+        obj = simplejson.loads(res_str)
+        self.assertEqual(obj["state"]["activeLayers"], [], obj)
+
+    def test_layers(self):	
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200, response.status_code)
+        res_str = response.content
+        obj = simplejson.loads(res_str)
+        self.assertEqual(obj["layers"][0]["name"], "Layer1")
+        for layerid in obj["themes"][0]["layers"]:
+            # make sure we can get the specified layer through the ORM
+            lyr = Layer.objects.get(id=layerid)
