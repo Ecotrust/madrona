d26a27e851c664e80c46757a62543dec63b577e8
Matthew Perry <mperry@ecotrust.org>
2012-10-23 19:20:08 -0700

    preliminary refactor of layer_manger

--- madrona/layer_manager/models.py
+++ madrona/layer_manager/models.py
@@ -1,18 +1,37 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Theme(models.Model):
+    display_name = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
+    header_image = models.CharField(max_length=255, blank=True, null=True)
+    header_attrib = models.CharField(max_length=255, blank=True, null=True)
+    overview = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    thumbnail = models.URLField(max_length=255, blank=True, null=True)
+    factsheet_thumb = models.CharField(max_length=255, blank=True, null=True)
+    factsheet_link = models.CharField(max_length=255, blank=True, null=True)
+    # not really using these atm    
+    feature_image = models.CharField(max_length=255, blank=True, null=True)
+    feature_excerpt = models.TextField(blank=True, null=True)
+    feature_link = models.CharField(max_length=255, blank=True, null=True)
 
     def __unicode__(self):
         return unicode('%s' % (self.name))
 
     @property
+    def learn_link(self):
+        return '/learn/%s' % (self.name)
+        
+    @property
     def toDict(self):
-        layers = [layer.id for layer in self.layer_set.filter(is_sublayer=False)]
+        layers = [layer.id for layer in self.layer_set.filter(is_sublayer=False).exclude(layer_type='placeholder')]
         themes_dict = {
             'id': self.id,
-            'name': self.name,
+            'display_name': self.display_name,
+            'learn_link': self.learn_link,
             'layers': layers,
+            'description': self.description
         }
         return themes_dict
 
@@ -20,42 +39,259 @@ class Layer(models.Model):
     TYPE_CHOICES = (
         ('XYZ', 'XYZ'),
         ('WMS', 'WMS'),
+        ('ArcRest', 'ArcRest'),
         ('radio', 'radio'),
+        ('checkbox', 'checkbox'),
         ('Vector', 'Vector'),
+        ('placeholder', 'placeholder'),
     )
     name = models.CharField(max_length=100)
     layer_type = models.CharField(max_length=50, choices=TYPE_CHOICES)
     url = models.CharField(max_length=255, blank=True, null=True)
-    sublayer = models.ManyToManyField('self', blank=True, null=True)
+    arcgis_layers = models.CharField(max_length=255, blank=True, null=True)
+    sublayers = models.ManyToManyField('self', blank=True, null=True)
     themes = models.ManyToManyField("Theme", blank=True, null=True)
     is_sublayer = models.BooleanField(default=False)
     legend = models.CharField(max_length=255, blank=True, null=True)
+    legend_title = models.CharField(max_length=255, blank=True, null=True)
+    legend_subtitle = models.CharField(max_length=255, blank=True, null=True)
     utfurl = models.CharField(max_length=255, blank=True, null=True)
-
+    
+    #tooltip
+    description = models.TextField(blank=True, null=True)
+    
+    #data description (updated fact sheet) (now the Learn pages)
+    data_overview = models.TextField(blank=True, null=True)
+    data_status = models.CharField(max_length=255, blank=True, null=True)
+    data_source = models.CharField(max_length=255, blank=True, null=True)
+    data_notes = models.TextField(blank=True, null=True)
+    
+    #data catalog links    
+    bookmark = models.CharField(max_length=755, blank=True, null=True)
+    map_tiles = models.CharField(max_length=255, blank=True, null=True)
+    kml = models.CharField(max_length=255, blank=True, null=True)
+    data_download = models.CharField(max_length=255, blank=True, null=True)
+    learn_more = models.CharField(max_length=255, blank=True, null=True)
+    metadata = models.CharField(max_length=255, blank=True, null=True)
+    fact_sheet = models.CharField(max_length=255, blank=True, null=True)
+    source = models.CharField(max_length=255, blank=True, null=True)
+    thumbnail = models.URLField(max_length=255, blank=True, null=True)
+    
+    #geojson javascript attribution
+    EVENT_CHOICES = (
+        ('click', 'click'),
+        ('mouseover', 'mouseover')
+    )
+    attribute_title = models.CharField(max_length=255, blank=True, null=True)
+    attribute_fields = models.ManyToManyField('AttributeInfo', blank=True, null=True)
+    compress_display = models.BooleanField(default=False)
+    attribute_event = models.CharField(max_length=35, choices=EVENT_CHOICES, default='click')
+    lookup_field = models.CharField(max_length=255, blank=True, null=True)
+    lookup_table = models.ManyToManyField('LookupInfo', blank=True, null=True)
+    vector_color = models.CharField(max_length=7, blank=True, null=True)
+    vector_fill = models.FloatField(blank=True, null=True)
+    vector_graphic = models.CharField(max_length=255, blank=True, null=True)
+    opacity = models.FloatField(default=.5, blank=True, null=True)
+    
     def __unicode__(self):
         return unicode('%s' % (self.name))
 
     @property
+    def is_parent(self):
+        return self.sublayers.all().count() > 0 and not self.is_sublayer
+    
+    @property
+    def parent(self):
+        if self.is_sublayer:
+            return self.sublayers.all()[0]
+        return self
+    
+    @property
+    def slug(self):
+        return slugify(self.name)
+
+    @property
+    def data_overview_text(self):
+        if not self.data_overview and self.is_sublayer:
+            return self.parent.data_overview
+        else:
+            return self.data_overview
+        
+    @property
+    def data_source_text(self):
+        if not self.data_source and self.is_sublayer:
+            return self.parent.data_source
+        else:
+            return self.data_source
+        
+    @property
+    def data_notes_text(self):
+        if not self.data_notes and self.is_sublayer:
+            return self.parent.data_notes
+        else:
+            return self.data_notes
+    
+    @property
+    def bookmark_link(self):
+        if not self.bookmark and self.is_sublayer and self.parent.bookmark:
+            return self.parent.bookmark.replace('<layer_id>', str(self.id))
+        else:
+            return self.bookmark
+    
+    @property
+    def data_download_link(self):
+        if self.data_download and self.data_download.lower() == 'none':
+            return None
+        if not self.data_download and self.is_sublayer:
+            return self.parent.data_download
+        else:
+            return self.data_download
+        
+    @property
+    def metadata_link(self):
+        if self.metadata and self.metadata.lower() == 'none':
+            return None
+        if not self.metadata and self.is_sublayer:
+            return self.parent.metadata
+        else:
+            return self.metadata
+        
+    @property
+    def source_link(self):
+        if self.source and self.source.lower() == 'none':
+            return None
+        if not self.source and self.is_sublayer:
+            return self.parent.source
+        else:
+            return self.source
+        
+    @property
+    def learn_link(self):
+        if self.learn_more:
+            return self.learn_more
+        else:
+            try:
+                theme = self.themes.all()[0]  
+            except IndexError:
+                return ""
+            return "%s#%s" %(theme.learn_link, self.slug)
+        
+    @property
+    def description_link(self):
+        theme_name = self.themes.all()[0].name
+        return '/learn/%s#%s' % (theme_name, self.slug)
+        
+    @property
+    def tooltip(self):
+        if self.description and self.description.strip() != '':
+            return self.description
+        elif self.parent.description and self.parent.description.strip() != '':
+            return self.parent.description
+        else:
+            return None
+            
+    @property
+    def serialize_attributes(self):
+        return {'title': self.attribute_title, 
+                'compress_attributes': self.compress_display,
+                'event': self.attribute_event,
+                'attributes': [{'display': attr.display_name, 'field': attr.field_name, 'precision': attr.precision} for attr in self.attribute_fields.all().order_by('order')]}
+    
+    @property
+    def serialize_lookups(self):
+        return {'field': self.lookup_field, 
+                'details': [{'value': lookup.value, 'color': lookup.color, 'dashstyle': lookup.dashstyle, 'fill': lookup.fill, 'graphic': lookup.graphic} for lookup in self.lookup_table.all()]}
+    
+    @property
     def toDict(self):
         sublayers = [
             {
+                'id': layer.id,
                 'name': layer.name,
                 'type': layer.layer_type,
                 'url': layer.url,
+                'arcgis_layers': layer.arcgis_layers,
                 'utfurl': layer.utfurl,
-                'id': layer.id,
                 'parent': self.id,
-                'legend': layer.legend 
+                'legend': layer.legend,
+                'legend_title': layer.legend_title,
+                'legend_subtitle': layer.legend_subtitle,
+                'description': layer.tooltip,
+                'learn_link': layer.learn_link,
+                'attributes': layer.serialize_attributes,
+                'lookups': layer.serialize_lookups,
+                'color': layer.vector_color,
+                'fill_opacity': layer.vector_fill,
+                'graphic': layer.vector_graphic,
+                'opacity': layer.opacity
             } 
-            for layer in self.sublayer.all()
+            for layer in self.sublayers.all()
         ]
         layers_dict = {
+            'id': self.id,
             'name': self.name,
             'type': self.layer_type,
             'url': self.url,
+            'arcgis_layers': self.arcgis_layers,
             'utfurl': self.utfurl,
             'subLayers': sublayers,
             'legend': self.legend,
-            'id': self.id
+            'legend_title': self.legend_title,
+            'legend_subtitle': self.legend_subtitle,
+            'description': self.description,
+            'learn_link': self.learn_link,
+            'attributes': self.serialize_attributes,
+            'lookups': self.serialize_lookups,
+            'color': self.vector_color,
+            'fill_opacity': self.vector_fill,
+            'graphic': self.vector_graphic,
+            'opacity': self.opacity
         }
         return layers_dict
+
+class AttributeInfo(models.Model):
+    display_name = models.CharField(max_length=255, blank=True, null=True)
+    field_name = models.CharField(max_length=255, blank=True, null=True)
+    precision = models.IntegerField(blank=True, null=True)
+    order = models.IntegerField(default=1)
+    
+    def __unicode__(self):
+        return unicode('%s' % (self.field_name)) 
+    
+class LookupInfo(models.Model):
+    DASH_CHOICES = (
+        ('dot', 'dot'),
+        ('dash', 'dash'),
+        ('dashdot', 'dashdot'),
+        ('longdash', 'longdash'),
+        ('longdashdot', 'longdashdot'),
+        ('solid', 'solid')
+    )
+    value = models.CharField(max_length=255, blank=True, null=True)
+    color = models.CharField(max_length=7, blank=True, null=True)
+    dashstyle = models.CharField(max_length=11, choices=DASH_CHOICES, default='solid')
+    fill = models.BooleanField(default=False)
+    graphic = models.CharField(max_length=255, blank=True, null=True)
+    
+    def __unicode__(self):
+        return unicode('%s' % (self.value)) 
+
+
+class DataNeed(models.Model):
+    name = models.CharField(max_length=100)
+    archived = models.BooleanField(default=False)
+    description = models.TextField(blank=True, null=True)
+    source = models.CharField(max_length=255, blank=True, null=True)
+    status = models.TextField(blank=True, null=True)
+    contact = models.CharField(max_length=255, blank=True, null=True)
+    contact_email = models.CharField(max_length=255, blank=True, null=True)
+    expected_date = models.CharField(max_length=255, blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+    themes = models.ManyToManyField("Theme", blank=True, null=True)
+
+    @property
+    def html_name(self):
+        return self.name.lower().replace(' ', '-')
+    
+    def __unicode__(self):
+        return unicode('%s' % (self.name))
---
+++ madrona/media/layer_manager/js/map.js
@@ -0,0 +1,302 @@
+app.viewModel.layers = {};
+
+app.utils.initMap = function (map) {
+
+    map.displayProjection = new OpenLayers.Projection("EPSG:4326");
+    map.projection = "EPSG:3857";
+
+    esriOcean = new OpenLayers.Layer.XYZ("ESRI Ocean", "http://services.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/${z}/${y}/${x}", {
+        sphericalMercator: true,
+        isBaseLayer: true,
+        numZoomLevels: 13,
+        attribution: "Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri"
+    });
+    
+    map.addLayers([esriOcean]);
+    esriOcean.setZIndex(100);
+
+    // TODO what is this? 
+    // map.addControl(new SimpleLayerSwitcher());
+
+    map.zoomBox = new OpenLayers.Control.ZoomBox();
+    map.addControl(map.zoomBox);
+
+    // only allow onetime zooming with box
+    map.events.register("zoomend", null, function () {
+        if (map.zoomBox.active) {
+            app.viewModel.layers.deactivateZoomBox();
+        }        
+    });
+
+    map.events.register("moveend", null, function () {
+        // update the url when we move
+        app.updateUrl();
+    });
+    
+
+    // callback functions for vector attribution (SelectFeature Control)
+    var report = function(e) {
+        var layer = e.feature.layer.layerModel;
+        
+        if ( layer.attributes.length ) {
+            var attrs = layer.attributes,
+                title = layer.name,
+                text = [];
+            app.viewModel.layers.attributeTitle(title); 
+            for (var i=0; i<attrs.length; i++) {
+                if ( e.feature.data[attrs[i].field] ) {
+                    text.push({'display': attrs[i].display, 'data': e.feature.data[attrs[i].field]});
+                }
+            }
+            app.viewModel.layers.attributeData(text);
+        }
+    };
+      
+    var clearout = function(e) {
+        app.viewModel.layers.attributeTitle(false);
+        app.viewModel.layers.attributeData(false);
+    };  
+    
+    map.vectorList = [];
+    map.selectFeatureControl = new OpenLayers.Control.SelectFeature(map.vectorList, {
+        //hover: true,
+        highlightOnly: false,
+        renderIntent: "temporary",
+        cancelBubble: false,
+        eventListeners: {
+            //beforefeaturehighlighted: report,
+            featurehighlighted: report,
+            //boxselectionend: report,
+            featureunhighlighted: clearout
+        }
+    });
+    map.addControl(map.selectFeatureControl);
+    map.selectFeatureControl.activate();  
+    
+    //UTF Attribution
+    map.UTFControl = new OpenLayers.Control.UTFGrid({
+        //attributes: layer.attributes,
+        layers: [],
+        handlerMode: 'click',
+        callback: function(infoLookup) {
+            app.viewModel.layers.attributeTitle(false);
+            app.viewModel.layers.attributeData(false);
+            if (infoLookup) {
+                var attributes;
+                $.each(app.viewModel.layers.visibleLayers(), function (layer_index, potential_layer) {
+                    if (!attributes) { //only loop if attributes has not yet been populated
+                        for (var idx in infoLookup) {
+                            if (!attributes) { //only loop if attributes has not yet been populated
+                                var info = infoLookup[idx];
+                                if (info && info.data) { 
+                                    var newmsg = '',
+                                        hasAllAttributes = true,
+                                        parentHasAllAttributes = false;
+                                    // if info.data has all the attributes we're looking for
+                                    // we'll accept this layer as the attribution layer 
+                                    if ( ! potential_layer.attributes.length ) {
+                                        hasAllAttributes = false;
+                                    }
+                                    $.each(potential_layer.attributes, function (attr_index, attr_obj) {
+                                        if ( !(attr_obj.field in info.data) ) {
+                                            hasAllAttributes = false;
+                                        }
+                                    });
+                                    if ( !hasAllAttributes && potential_layer.parent) {
+                                        parentHasAllAttributes = true;
+                                        if ( ! potential_layer.parent.attributes.length ) {
+                                            parentHasAllAttributes = false;
+                                        }
+                                        $.each(potential_layer.parent.attributes, function (attr_index, attr_obj) {
+                                            if ( !(attr_obj.field in info.data) ) {
+                                                parentHasAllAttributes = false;
+                                            }
+                                        });
+                                    }
+                                    if (hasAllAttributes) {
+                                        attributes = potential_layer.attributes;
+                                    } else if (parentHasAllAttributes) {
+                                        attributes = potential_layer.parent.attributes;
+                                    }
+                                    if (attributes) { 
+                                        var attribute_objs = [];
+                                        $.each(attributes, function(index, obj) {
+                                            if ( potential_layer.compress_attributes ) {
+                                                var display = obj.display + ': ' + info.data[obj.field];
+                                                attribute_objs.push({'display': display, 'data': ''});
+                                            } else {
+                                                /*** SPECIAL CASE FOR ENDANGERED WHALE DATA ***/
+                                                var value = info.data[obj.field];
+                                                if (value === 999999) {
+                                                    attribute_objs.push({'display': obj.display, 'data': 'No Survey Effort'});
+                                                } else {
+                                                    try {
+                                                        value = value.toFixed(obj.precision);
+                                                    }
+                                                    catch (e) {
+                                                        //keep on keeping on
+                                                    }
+                                                    attribute_objs.push({'display': obj.display, 'data': value});
+                                                }
+                                                
+                                            }
+                                        });
+                                        app.viewModel.layers.attributeTitle(potential_layer.name);
+                                        //app.viewModel.layers.attributeData([{'display': potential_layer.attributeTitle, 'data': newmsg}]);
+                                        app.viewModel.layers.attributeData(attribute_objs);
+                                    } 
+                                } 
+                            }
+                        }
+                    }
+                });
+            } 
+        }
+    });
+    map.addControl(map.UTFControl);    
+
+    app.map = map;
+};
+
+app.utils.addLayerToMap = function(layer) {
+    if (!layer.layer) {
+        var opts = {
+            displayInLayerSwitcher: false
+        };
+        
+        if (layer.utfurl || (layer.parent && layer.parent.utfurl)) {
+            layer.utfgrid = new OpenLayers.Layer.UTFGrid({
+                layerModel: layer,
+                url: layer.utfurl ? layer.utfurl : layer.parent.utfurl,
+                sphericalMercator: true,
+                                 
+                utfgridResolution: 4, // default is 2
+                displayInLayerSwitcher: false,
+                useJSONP: false
+            });
+            //layer.utfgrid.projection = new OpenLayers.Projection("EPSG:4326");  
+            app.map.addLayer(layer.utfgrid);           
+            //app.map.UTFControl.layers = [layer.utfgrid];
+            //layer.utfcontrol = app.addUTFControl(layer);
+            //app.map.addControl(layer.utfcontrol); 
+            layer.layer = new OpenLayers.Layer.XYZ(
+                layer.name, 
+                layer.url,
+                $.extend({}, opts, 
+                    {
+                        sphericalMercator: true,
+                        isBaseLayer: false //previously set automatically when allOverlays was set to true, must now be set manually
+                    }
+                )
+            );  
+            app.map.addLayer(layer.layer);  
+            //app.addUTFAttribution(layer);
+        } else if (layer.type === 'Vector') {
+            var styleMap = new OpenLayers.StyleMap( {
+                fillColor: layer.color,
+                fillOpacity: layer.fillOpacity,
+                //strokeDashStyle: "dash",
+                //strokeOpacity: 1,
+                strokeColor: layer.color,
+                strokeOpacity: layer.defaultOpacity,
+                //strokeLinecap: "square",
+                //http://dev.openlayers.org/apidocs/files/OpenLayers/Feature/Vector-js.html
+                //title: 'testing'
+                pointRadius: 2,
+                externalGraphic: layer.graphic,
+                graphicWidth: 8,
+                graphicHeight: 8,
+                graphicOpacity: layer.defaultOpacity
+            });
+            if (layer.lookupField) {
+                var mylookup = {};
+                $.each(layer.lookupDetails, function(index, details) {                  
+                    mylookup[details.value] = { strokeColor: details.color, 
+                                                strokeDashstyle: details.dashstyle, 
+                                                fill: details.fill,
+                                                fillColor: details.color, 
+                                                fillOpacity: 0.5,
+                                                externalGraphic: details.graphic }; 
+                });
+                styleMap.addUniqueValueRules("default", layer.lookupField, mylookup);
+                //styleMap.addUniqueValueRules("select", layer.lookupField, mylookup);
+            }
+            layer.layer = new OpenLayers.Layer.Vector(
+                layer.name,
+                {
+                    projection: new OpenLayers.Projection('EPSG:3857'),
+                    displayInLayerSwitcher: false,
+                    strategies: [new OpenLayers.Strategy.Fixed()],
+                    protocol: new OpenLayers.Protocol.HTTP({
+                        url: layer.url,
+                        format: new OpenLayers.Format.GeoJSON()
+                    }),
+                    styleMap: styleMap,                    
+                    layerModel: layer
+                }
+            );
+            //app.addVectorAttribution(layer);
+            app.map.addLayer(layer.layer);  
+            //selectFeatureControl = app.map.getControlsByClass("OpenLayers.Control.SelectFeature")[0];
+            if (layer.attributes.length) {
+                app.map.vectorList.unshift(layer.layer);
+                app.map.selectFeatureControl.setLayer(app.map.vectorList);
+            }
+        } else if (layer.type === 'ArcRest') {
+            layer.layer = new OpenLayers.Layer.ArcGIS93Rest(
+                layer.name, 
+                layer.url,
+                {
+                    layers: "show:"+layer.arcgislayers,
+                    srs: 'EPSG:3857',
+                    transparent: true
+                },
+                {
+                    isBaseLayer: false
+                }
+            );
+            app.map.addLayer(layer.layer);  
+        } else if (layer.type === 'WMS') {
+            layer.layer = new OpenLayers.Layer.WMS(
+                layer.name, 
+                layer.url,
+                {
+                    'layers': 'basic'
+                }
+            );
+            app.map.addLayer(layer.layer);  
+        } else { //if XYZ with no utfgrid
+            // adding layer to the map for the first time		
+            layer.layer = new OpenLayers.Layer.XYZ(layer.name, 
+                //layer.type === 'XYZ' ? layer.url : layer.url + '.png', 
+                layer.url,
+                $.extend({}, opts, 
+                    {
+                        sphericalMercator: true,
+                        isBaseLayer: false //previously set automatically when allOverlays was set to true, must now be set manually
+                    }
+                )
+            );
+            app.map.addLayer(layer.layer);  
+        }
+        //app.map.addLayer(layer.layer);  
+        //layer.layer.projection = new OpenLayers.Projection("EPSG:3857");
+    } else if ( layer.utfurl ) { //re-adding utfcontrol for existing utf layers (they are destroyed in layer.deactivateLayer)
+        //layer.utfcontrol = app.addUTFControl(layer);
+        //app.map.addControl(layer.utfcontrol); 
+    }
+    layer.layer.opacity = layer.opacity();
+    layer.layer.setVisibility(true);
+};
+
+
+app.utils.setLayerVisibility = function(layer, visibility) {
+    // if layer is in openlayers, hide it
+    if (layer.layer) {
+        layer.layer.setVisibility(visibility);
+    }
+};
+
+app.utils.setLayerZIndex = function(layer, index) {
+    layer.layer.setZIndex(index);
+};
