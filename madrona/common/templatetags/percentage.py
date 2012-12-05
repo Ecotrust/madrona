0f2cb34d5136477886590bc3606702bddba7fc4d
Matthew Perry <mperry@ecotrust.org>
2012-12-05 13:06:00 -0800

    Flexible typing in percentage template filter

--- madrona/common/templatetags/percentage.py
+++ madrona/common/templatetags/percentage.py
@@ -3,7 +3,8 @@ from django import template
 register = template.Library()
 
 def percentage(decimal, precision=0):
+    d = float(decimal)
     format_string = "." + str(precision) + "%"
-    return format(decimal, format_string)
+    return format(d, format_string)
 
 register.filter('percentage', percentage)
