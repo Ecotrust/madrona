47488373b90b56915026e6393b808a5cba77b0a1
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-04-17 15:38:53 -0700

    fixing OL Admin widget zoom

--- madrona/static/gis/js/OLMapWidget.js
+++ madrona/static/gis/js/OLMapWidget.js
@@ -102,7 +102,7 @@ ol.inherits(GeometryTypeControl, ol.control.Control);
                 ol.extent.extend(extent, feature.getGeometry().getExtent());
             }, this);
             // Center/zoom the map
-            this.map.getView().fit(extent, {maxZoom: this.options.default_zoom});
+            this.map.getView().fit(extent);
         } else {
             this.map.getView().setCenter(this.defaultCenter());
         }
